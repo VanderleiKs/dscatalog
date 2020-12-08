1fbca94fac025a8d4ad37aa8ddd643a16147c18e
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-12-07 23:00:27 -0300

    create front-end

---
+++ dscatalog_frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Http2ServerRequest } from 'http2';
+import React from 'react';
+import './core/assets/style/custom.scss';
+
+
+function App() {
+
+  return (
+    <h1>ola mundo</h1>
+  );
+}
+
+export default App;
